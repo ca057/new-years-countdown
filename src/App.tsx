adc81fd06e7f19430e7faa528f679b0c27473840
Johannes Trepesch <HannesT117@@users.noreply.github.com>
2022-12-31 16:57:18 +0100

    adjust celebration trigger

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import queryString from "query-string";
 
 const formatTimeElement = (val: number) => val.toString().padStart(2, "0");
 
-type RemainingTime = [number, number, number];
+type RemainingTime = [number, number, number, number];
 
 type CountdownProps = {
   target: DateTime;
@@ -16,14 +16,16 @@ type CountdownProps = {
 
 const Countdown = ({ target }: CountdownProps) => {
   const initialDiff = useRef(target.diffNow().shiftToAll().toObject()).current;
-  const [[hours, minutes, seconds], setNextTime] = useState<RemainingTime>([
+  const [[hours, minutes, seconds, milliseconds], setNextTime] = useState<RemainingTime>([
     initialDiff.hours ?? 0,
     initialDiff.minutes ?? 0,
     initialDiff.seconds ?? 0,
+    initialDiff.milliseconds ?? 0,
   ]);
   const interval = useRef<ReturnType<typeof setInterval>>();
 
-  const celebrate = hours <= 0 && minutes <= 0 && seconds <= 0;
+  const cantWait = hours <= 0 && minutes <= 0 && seconds <= 20;
+  const celebrate = hours <= 0 && minutes <= 0 && seconds <= 0 && milliseconds <= 0;
 
   useEffect(() => {
     if (celebrate) {
@@ -36,6 +38,7 @@ const Countdown = ({ target }: CountdownProps) => {
         clamp(diff.hours ?? 0, 0, 24),
         clamp(diff.minutes ?? 0, 0, 60),
         clamp(diff.seconds ?? 0, 0, 60),
+        clamp(diff.milliseconds ?? 0, 0, 1000),
       ]);
     }, 500);
 
@@ -100,6 +103,49 @@ const Countdown = ({ target }: CountdownProps) => {
 
   return (
     <>
+    {cantWait &&  <Fireworks
+          options={{
+            opacity: 1,
+            acceleration: 1.02,
+            friction: 0.97,
+            gravity: 2,
+            particles: 200,
+            explosion: 6,
+            delay: {
+              min: 30,
+              max: 60,
+            },
+            brightness: {
+              min: 80,
+              max: 100,
+            },
+            decay: {
+              min: 0.01,
+              max: 0.02,
+            },
+            flickering: 60,
+            intensity: 1,
+            traceSpeed: 1,
+            lineWidth: {
+              explosion: {
+                min: 5,
+                max: 7,
+              },
+              trace: {
+                min: 3,
+                max: 4,
+              },
+            },
+            autoresize: true,
+          }}
+          style={{
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            position: "fixed",
+          }}
+        />}
       <div className="countdown">
         <span className="number">{formatTimeElement(hours)}</span>
         <span>:</span>
